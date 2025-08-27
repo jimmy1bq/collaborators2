f75abcac8fd7de68eb27bfa91194991e3cd292c1
jimmy <jimmyzheng8298@gmail.com>
2025-08-27 18:56:02 -0400

    UH

--- SpaceShooterThing/Assets/Scripts/PlayerScript.cs
+++ SpaceShooterThing/Assets/Scripts/PlayerScript.cs
@@ -5,16 +5,15 @@ public class Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Player script has Joined.");
+
     }
     // Update is called once per frame
     void Update()
     {
        Vector3 comps = Camera.main.ScreenToViewportPoint(Input.mousePosition);
         transform.position = new Vector3(
-        comps.x, 
-        comps.y, 0);
-       Debug.Log(comps.x);
-        Debug.Log(comps.y);
+        comps.x + comps.x+ comps.x + comps.x, 
+        comps.y+ comps.y + comps.y + comps.y, 0);
+      
     }
 }
